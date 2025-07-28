ad5be382658e05f991d0a6c301d6df3fcbc8a338
ettore-infn <ettore.serra@ba.infn.it>
2025-07-28 09:22:01 +0000

    added default to rse slas map

--- app/deployments/routes.py
+++ app/deployments/routes.py
@@ -1802,8 +1802,12 @@ def map_slas_rse(slas):
                 
             # VENETO
             case ("regionOne", "CLOUD-VENETO"):
-                sla["rses"] = ["LNL_USERDISK", "CLOUDVENETO_USERDISK"]          
-        
+                sla["rses"] = ["LNL_USERDISK", "CLOUDVENETO_USERDISK"]         
+                
+            # DEFAULT
+            case _:
+                sla["rses"] = []
+
     return slas
 
 
